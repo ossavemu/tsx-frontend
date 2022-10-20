9a70e50a16a00a48676a20831a3962dd8ed1dab5
OSCAR SANTIAGO VELEZ MUÑOZ <90299049+ossavemu@users.noreply.github.com>
2022-10-20 12:50:35 -0500

    NavBar template implementation

---
+++ rebuild-tsx-frontend/src/Components/Layout.tsx
@@ -0,0 +1,22 @@
+import { Box, Center, Divider, Text } from '@chakra-ui/react'
+
+const Layout = () => {
+  return (
+    <Box w="100%" padding="4">
+      <Text>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque
+        similique maxime doloribus nihil vitae, neque temporibus voluptatibus
+        repellendus! Id quod dignissimos architecto maiores excepturi nam eum
+        saepe ullam necessitatibus illo? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Excepturi veniam impedit perferendis iusto
+        consequuntur quisquam blanditiis pariatur molestias earum ducimus odit,
+        possimus est, iste mollitia error dolorem totam suscipit numquam. Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Ipsa natus quos odit
+        ullam dolorem, eaque aliquid et, pariatur repudiandae eum quo quis aut
+        magni iste, explicabo numquam? Dignissimos, laudantium ut.
+      </Text>
+    </Box>
+  )
+}
+
+export default Layout
---
+++ rebuild-tsx-frontend/src/Components/Navbar/Logo.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from '@chakra-ui/react'
+
+const Logo = (props: any) => {
+  return (
+    <Box {...props}>
+      <Text fontSize="lg" fontWeight="bold">
+        Logo
+      </Text>
+    </Box>
+  )
+}
+export default Logo
